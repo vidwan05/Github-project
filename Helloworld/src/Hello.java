afc42486a3b28bc1dee6901d53d8f8fa189bd198
vidwan05 <33649368+vidwan05@users.noreply.github.com>
2018-01-06 14:36:34 -0600

    Github-project
    Github-project

---
+++ Helloworld/src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Vidwan Reddy on 11-07-2017.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello vidwan!!");
+        int myfirstnumber = (5+8) + (9+90);
+        int mysecondnumber = 10;
+        int mythirdnumber = myfirstnumber + 90;
+        int mytotal = myfirstnumber + mysecondnumber + mythirdnumber;
+        System.out.println(mytotal);
+    }
+}
